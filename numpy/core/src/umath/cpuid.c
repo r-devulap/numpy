15cda71efe465a9ab7fd40c1c26c56bbae4ad591
Raghuveer Devulapalli <raghuveer.devulapalli@intel.com>
2019-07-20 12:30:19 -0700

    BUG: Fixing build fail on ICC 2016 compiler
    Getting rid of __builtin_cpu_supports for avx512f and implementing cpuid
    directly. CPUID check is implemented for x86 64 bit architecture, not
    sure how to implement it on 32-bit.

--- numpy/core/src/umath/cpuid.c
+++ numpy/core/src/umath/cpuid.c
@@ -13,6 +13,30 @@
 #define XSTATE_YMM 0x4
 #define XSTATE_ZMM 0x70
 
+static NPY_INLINE
+void npy_cpuid(uint32_t leaf,
+               uint32_t* eax,
+               uint32_t* ebx,
+               uint32_t* ecx,
+               uint32_t* edx)
+{
+#if defined(__amd64__) || defined(__x86_64__) || defined(_M_AMD64)
+#if defined __GNUC__
+    __asm__ volatile (
+      "cpuid"				"\n\t"
+	: "=a" (*eax), "=b" (*ebx), "=c" (*ecx), "=d" (*edx)
+	: "a" (leaf), "c" (0));
+#elif defined (_MSC_VER)
+    int info[4];
+    __cpuid (info, leaf);
+    *eax = info[0];
+    *ebx = info[1];
+    *ecx = info[2];
+    *edx = info[3];
+#endif
+#endif
+}
+
 /*
  * verify the OS supports avx instructions
  * it can be disabled in some OS, e.g. with the nosavex boot option of linux
@@ -48,6 +72,18 @@ int os_avx512_support(void)
 #endif
 }
 
+static NPY_INLINE
+uint32_t cpuid_supports_avx512f(void)
+{
+    /*
+     * Call 7th leaf of CPUID
+     * CPUID sets the 17th bit of EBX register if AVX512F is supported
+     */
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+    uint32_t leaf = 0x07;
+    npy_cpuid(leaf, &eax, &ebx, &ecx, &edx);
+    return (ebx >> 16) & 0x01;
+}
 /*
  * Primitive cpu feature detect function
  * Currently only supports checking for avx on gcc compatible compilers.
@@ -60,7 +96,7 @@ npy_cpu_supports(const char * feature)
 #if defined(__GNUC__) && (__GNUC__ < 5)
         return 0;
 #else
-        return __builtin_cpu_supports("avx512f") && os_avx512_support();
+        return cpuid_supports_avx512f() && os_avx512_support();
 #endif
     }
     else if (strcmp(feature, "avx2") == 0) {
